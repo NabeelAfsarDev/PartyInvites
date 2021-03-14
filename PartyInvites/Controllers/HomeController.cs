770c501980d01cd317c71113f34eafcd9dc51cf8
Nabeel Afsar <na.developer786@outlook.com>
2021-03-14 13:39:33 -0500

    Initial RSVP form controller and view created

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -9,7 +9,14 @@ namespace PartyInvites.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+            return View();
+        }
+
+        public ViewResult RsvpForm()
         {
             return View();
         }
